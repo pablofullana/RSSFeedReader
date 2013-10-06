fe4ca3a3a38e98fe83cc20dd09b2bf6e0dbcfdb4
pablofullana <pablofullana@gmail.com>
2013-10-06 13:39:44 -0300

    Fixed extra user information migration. Rollback is now supported

--- db/migrate/20131005212845_add_login_first_name_last_nameto_user.rb
+++ db/migrate/20131005212845_add_login_first_name_last_nameto_user.rb
@@ -1,7 +1,12 @@
 class AddLoginFirstNameLastNametoUser < ActiveRecord::Migration
-  def change
+  def self.up
   	add_column :users, :login, :string, :null => false, :default => ""
   	add_column :users, :first_name, :string, :null => false, :default => ""
     add_column :users, :last_name, :string, :null => false, :default => ""
+  end
+  def self.down
+  	remove_column :users, :login
+  	remove_column :users, :first_name
+    remove_column :users, :last_name
   end
 end
